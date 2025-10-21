14bfce8cb29658944d3133a68b4caf57dfdf3e8a
Alexis <alexisvignot@hotmail.fr>
2025-10-21 10:47:23 +0200

    fix: EffectComposer disabled on mobile

--- src/components/v2/Presentation/Presentation.tsx
+++ src/components/v2/Presentation/Presentation.tsx
@@ -10,6 +10,7 @@ import './utils.ts';
 import type { JSX } from "react/jsx-runtime";
 import { InstancesContext, type GLTFNodes, type InstancedComponents, type ObjectProps } from './InstancesContext'; // adapte le chemin si besoin
 import { useInstances } from './useInstanecs';
+import { useDeviceType } from "../../../hooks/useDeviceType.ts";
 type GroupProps = JSX.IntrinsicElements['group'];
 
 
@@ -356,9 +357,8 @@ export function Leds({ instances }: LedsProps) {
 export function SafePostEffects({ activeScene, onComposerReady }: { activeScene: number; onComposerReady?: () => void }) {
   const { camera, gl, size } = useThree();
   const [ready, setReady] = useState(false);
+  const device = useDeviceType();
 
-  // const bloom = useDampedValue(activeScene === 0 ? 1.5 : 0);
-  // const bokeh = useDampedValue(activeScene === 0 ? 10 : 0);
   const bloom = activeScene === 0 ? 1.5 : 0;
   const bokeh = activeScene === 0 ? 10 : 0;
 
@@ -380,6 +380,8 @@ export function SafePostEffects({ activeScene, onComposerReady }: { activeScene:
   }, [camera]);
 
   if (!ready) return null;
+
+  if (device.device === 'mobile') return null;
 
   return (
     <EffectComposer enableNormalPass={false}>
@@ -404,40 +406,31 @@ export default function Presentation({ activeScene, position, onComposerReady }:
   return (
     <group position={position || [0, 0, 0]}>
       {/* Lights */}
-      {activeScene === 0 && (
-        <>
-          <color attach="background" args={['black']} />
-          <hemisphereLight intensity={0.15} groundColor="black" />
-          <spotLight decay={0} position={[10, 20, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-        </>
-      )}
+      <color attach="background" args={['black']} />
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight decay={0} position={[10, 20, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
       {/* Main scene */}
       <group position={[-0, -1, 0]}>
         {/* Auto-instanced sketchfab model */}
         <Instances>
           <Computers scale={0.5} />
         </Instances>
-        {
-          activeScene === 0 && (
-            <>
-              {/* Plane reflections + distance blur */}
-              <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-                <planeGeometry args={[50, 50]} />
-                <MeshReflectorMaterial
-                  blur={[300, 30]}
-                  resolution={2048}
-                  mixBlur={1}
-                  mixStrength={180}
-                  roughness={1}
-                  depthScale={1.2}
-                  minDepthThreshold={0.4}
-                  maxDepthThreshold={1.4}
-                  color="#202020"
-                  metalness={0.8}
-                />
-              </mesh>
-            </>
-          )}
+        {/* Plane reflections + distance blur */}
+        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[50, 50]} />
+          <MeshReflectorMaterial
+            blur={[300, 30]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={180}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#202020"
+            metalness={0.8}
+          />
+        </mesh>
       </group>
       {
 
