373a42285d06b8a984e4220a4fe880ee840fa444
Alexis Vignot <alexisvignot@hotmail.fr>
2026-03-16 15:46:14 +0100

    fix: package json

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +7,9 @@ export default defineConfig({
     react()
   ],
   base: '/',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
 })
